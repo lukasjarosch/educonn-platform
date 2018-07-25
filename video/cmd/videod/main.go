f72c09b704554fbc171dad5825c665363dfd09b4
Lukas Jarosch <lukas.jarosch@mail.com>
2018-07-25 23:24:59 +0200

    started video service implementation
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- video/cmd/videod/main.go
+++ video/cmd/videod/main.go
@@ -1,1 +1,35 @@
-package videod
+package main
+
+import (
+	"github.com/micro/go-micro"
+	"github.com/lukasjarosch/educonn-master-thesis/video/internal/platform/config"
+	"time"
+	"github.com/micro/go-plugins/broker/rabbitmq"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+
+	// setup micro service
+	svc := micro.NewService(
+		micro.Name(config.ServiceName),
+		micro.Version(config.Version),
+		micro.RegisterTTL(time.Second*30),
+		micro.RegisterInterval(time.Second*10),
+	)
+	svc.Init()
+
+	// setup rabbitmq
+	rabbitBroker := svc.Server().Options().Broker
+	if err := rabbitBroker.Init(rabbitmq.Exchange("educonn")); err != nil {
+		log.Fatalf("Broker Init error: %v", err)
+	}
+	if err := rabbitBroker.Connect(); err != nil {
+		log.Fatalf("Broker Connect error: %v", err)
+	}
+	micro.Broker(rabbitBroker)
+
+	if err := svc.Run(); err != nil {
+		panic(err)
+	}
+}
--- video/internal/platform/config/version.go
+++ video/internal/platform/config/version.go
@@ -1,1 +1,9 @@
 package config
+
+// Version is used to define the application version and should be overridden during the
+// compilation phase.
+var Version = "(undefined)"
+
+
+// ServiceName is the discovery identity for this service.
+const ServiceName = "educonn.video"
