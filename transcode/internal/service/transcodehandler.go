f885b101af9743ba7d6a6fcdc02e641efd790881
Lukas Jarosch <lukas.jarosch@mail.com>
2018-07-28 19:39:40 +0200

    Transcode can now start jobs
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

---
+++ transcode/internal/service/transcodehandler.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"github.com/lukasjarosch/educonn-master-thesis/video/internal/platform/amazon"
+	"github.com/prometheus/common/log"
+)
+
+// transcodeHandler implements the ElasticTranscodeEvendHandler interface
+type transcodeHandler struct {
+}
+
+func NewTranscodeHandler() *transcodeHandler {
+	return &transcodeHandler{}
+}
+
+func (t *transcodeHandler) OnCompleted(message *amazon.ElasticTranscoderMessage) error {
+	log.Infof("[ElasticTranscoder] COMPLETED job '%s' on pipeline '%s': %s", message.JobId, message.PipelineId, message.Outputs[0].Key)
+	return nil
+}
+
+func (t *transcodeHandler) OnWarning(message *amazon.ElasticTranscoderMessage) error {
+	log.Warn(message.MessageDetails)
+	return nil
+}
+
+func (t *transcodeHandler) OnError(message *amazon.ElasticTranscoderMessage) error {
+	log.Info("OnError")
+	return nil
+}
