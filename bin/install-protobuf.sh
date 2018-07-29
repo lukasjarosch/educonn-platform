e94fde338f24ad6257dc45ff5d29007007f72f4d
Lukas Jarosch <lukas.jarosch@mail.com>
2018-07-29 19:28:10 +0200

    adjustments on PATH for protobuf
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- bin/install-protobuf.sh
+++ bin/install-protobuf.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 wget https://github.com/google/protobuf/releases/download/v3.6.0/protoc-3.6.0-linux-x86_64.zip
-unzip protoc-3.6.0-linux-x86_64.zip -d "/usr/bin/protobuf"
+unzip protoc-3.6.0-linux-x86_64.zip -d "${HOME}/protobuf"
