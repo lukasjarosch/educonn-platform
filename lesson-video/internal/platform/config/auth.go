9914f3cf8bb158fad6443442acd8727b21529355
Lukas Jarosch <lukas.jarosch@mail.com>
2018-08-10 18:44:17 +0200

    for now we can keep the services inside the lesson
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- lesson-video/internal/platform/config/auth.go
+++
@@ -1,7 +0,0 @@
-package config
-
-import "os"
-
-var (
-	PublicKeyPath    = os.Getenv("AUTH_PUBLIC_KEY_PATH")
-)
--- lesson-video/internal/platform/config/version.go
+++
@@ -1,8 +0,0 @@
-package config
-
-// Version is used to define the application version and should be overridden during the
-// compilation phase.
-var Version = "(undefined)"
-
-// ServiceName is the discovery identity for this service.
-const ServiceName = "educonn.srv.video-lesson"
