ff689f0fd7216ae1f6d5370a32f64e638d5b7507
Lukas Jarosch <lukas.jarosch@mail.com>
2018-09-16 02:02:58 +0200

    instrumented user-api; also added some event tags to the spans
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- user-api/cmd/user-api/main.go
+++ user-api/cmd/user-api/main.go
@@ -7,14 +7,16 @@ import (
 	"github.com/micro/go-micro"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	zipkin "github.com/openzipkin/zipkin-go-opentracing"
+	opentracing "github.com/opentracing/opentracing-go"
+	ot "github.com/micro/go-plugins/wrapper/trace/opentracing"
 
-	"github.com/lukasjarosch/educonn-platform/user-api/internal/middleware"
 	"github.com/lukasjarosch/educonn-platform/user-api/internal/platform/config"
 	"github.com/lukasjarosch/educonn-platform/user/pkg/jwt_handler"
 
-	pbUser"github.com/lukasjarosch/educonn-platform/user/proto"
-	pbVideo "github.com/lukasjarosch/educonn-platform/video/proto"
 	api "github.com/lukasjarosch/educonn-platform/user-api/internal/service"
+	pbUser "github.com/lukasjarosch/educonn-platform/user/proto"
+	pbVideo "github.com/lukasjarosch/educonn-platform/video/proto"
 )
 
 func main() {
@@ -22,20 +24,23 @@ func main() {
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	}
 
+	InitTracer("http://localhost:9411/api/v1/spans", "9411", config.ServiceName)
+
 	// setup service
 	service := micro.NewService(
 		micro.Name(config.ServiceName),
-		micro.WrapHandler(middleware.TraceHandlerWrapper),
 		micro.Version(config.Version),
 		micro.RegisterTTL(time.Second*30),
 		micro.RegisterInterval(time.Second*10),
+		micro.WrapHandler(ot.NewHandlerWrapper(opentracing.GlobalTracer())),
+		micro.WrapClient(ot.NewClientWrapper(opentracing.GlobalTracer())),
 	)
 	service.Init()
 
 	// jwt handler
 	jwtService, err := jwt_handler.NewJwtTokenHandler(config.PublicKeyPath, "")
 	if err != nil {
-	    log.Fatal().Err(err).Str("AUTH_PUBLIC_KEY_PATH", config.PublicKeyPath).Msg("unable to create JwtTokenHandler")
+		log.Fatal().Err(err).Str("AUTH_PUBLIC_KEY_PATH", config.PublicKeyPath).Msg("unable to create JwtTokenHandler")
 	}
 
 	// rpc clients
@@ -48,4 +53,22 @@ func main() {
 	if err := service.Run(); err != nil {
 		panic(err)
 	}
+}
+
+func InitTracer(zipkinURL string, hostPort string, serviceName string) {
+	log.Debug().Msg("Initialize tracing")
+	collector, err := zipkin.NewHTTPCollector(zipkinURL)
+	if err != nil {
+		log.Error().Msgf("unable to create Zipkin HTTP collector: %v", err)
+		return
+	}
+	tracer, err := zipkin.NewTracer(
+		zipkin.NewRecorder(collector, true, hostPort, serviceName),
+	)
+	if err != nil {
+		log.Error().Msgf("unable to create Zipkin tracer: %v", err)
+		return
+	}
+	opentracing.InitGlobalTracer(tracer)
+	return
 }
--- user-api/internal/service/service.go
+++ user-api/internal/service/service.go
@@ -2,30 +2,34 @@ package service
 
 import (
 	"context"
+	"net/mail"
+
+	"github.com/lukasjarosch/educonn-platform/user-api/internal/platform/config"
+	"github.com/lukasjarosch/educonn-platform/user-api/internal/platform/errors"
 	pb "github.com/lukasjarosch/educonn-platform/user-api/proto"
+	"github.com/lukasjarosch/educonn-platform/user/pkg/jwt_handler"
 	pbUser "github.com/lukasjarosch/educonn-platform/user/proto"
 	pbVideo "github.com/lukasjarosch/educonn-platform/video/proto"
 	merr "github.com/micro/go-micro/errors"
-	"github.com/rs/zerolog/log"
-	"net/mail"
 	"github.com/micro/go-micro/metadata"
-	"github.com/lukasjarosch/educonn-platform/user-api/internal/platform/config"
-	"github.com/lukasjarosch/educonn-platform/user-api/internal/platform/errors"
-	"github.com/lukasjarosch/educonn-platform/user/pkg/jwt_handler"
+	"github.com/opentracing/opentracing-go"
+	"github.com/rs/zerolog/log"
 )
 
 type User struct {
-	userClient pbUser.UserClient
+	userClient  pbUser.UserClient
 	videoClient pbVideo.VideoClient
-	jwtService *jwt_handler.JwtTokenHandler
+	jwtService  *jwt_handler.JwtTokenHandler
 }
 
 func NewUserApi(userClient pbUser.UserClient, videoClient pbVideo.VideoClient, jwtService *jwt_handler.JwtTokenHandler) *User {
-	return &User{userClient: userClient, videoClient: videoClient, jwtService:jwtService}
+	return &User{userClient: userClient, videoClient: videoClient, jwtService: jwtService}
 }
 
 func (u *User) Create(ctx context.Context, req *pb.CreateRequest, res *pb.CreateResponse) error {
 
+	span := opentracing.SpanFromContext(ctx)
+	span.SetOperationName("Create")
 
 	if req.User == nil {
 		return merr.BadRequest(config.ServiceName, "%s", "Request contains no data")
@@ -54,11 +58,16 @@ func (u *User) Create(ctx context.Context, req *pb.CreateRequest, res *pb.Create
 		return merr.BadRequest(config.ServiceName, "%s", "Please specify a password")
 	}
 
+	span.LogKV("event", "call userClient.Create")
+
 	user, err := u.userClient.Create(ctx, req.User)
 	if err != nil {
 		log.Warn().Interface("error", err).Msg("unable to create user")
+		span.LogKV("event", "userClient.Create failed")
 		return merr.InternalServerError(config.ServiceName, "%s: %s", "Unable to create user", err.Error())
 	}
+
+	span.LogKV("event", "userClient.Create finished")
 
 	res.User = user.User
 
@@ -66,23 +75,27 @@ func (u *User) Create(ctx context.Context, req *pb.CreateRequest, res *pb.Create
 }
 
 func (u *User) Delete(ctx context.Context, req *pb.DeleteRequest, res *pb.DeleteResponse) error {
+	span := opentracing.SpanFromContext(ctx)
+	span.SetOperationName("delete")
 
 	md, _ := metadata.FromContext(ctx)
 
 	// check for token
 	token, err := u.jwtService.GetBearerToken(md)
 	if err != nil {
-	    log.Error().Interface("err", err).Msg("asdf")
-	    return merr.Unauthorized(config.ServiceName, "%s", err.Error())
+		span.LogKV("event", "missing JWT token")
+		return merr.Unauthorized(config.ServiceName, "%s", err.Error())
 	}
 
 	// validate jwt
 	_, err = u.jwtService.Decode(token)
 	if err != nil {
-	    log.Error().Interface("error", err).Msg(errors.InvalidJWTToken.Error())
-	    return merr.Unauthorized(config.ServiceName, "%s", errors.InvalidJWTToken)
+		log.Error().Interface("error", err).Msg(errors.InvalidJWTToken.Error())
+		span.LogKV("event", "invalid jwt token")
+		return merr.Unauthorized(config.ServiceName, "%s", errors.InvalidJWTToken)
 	}
 
+	span.LogKV("event", "call userClient.Delete")
 
 	// delete user
 	_, err = u.userClient.Delete(ctx, &pbUser.DeleteRequest{
@@ -95,10 +108,14 @@ func (u *User) Delete(ctx context.Context, req *pb.DeleteRequest, res *pb.Delete
 	}
 	log.Debug().Str("user", req.UserId).Msg("deleted user from database")
 
+	span.LogKV("event", "userClient.Delete finished")
+
 	return nil
 }
 
 func (u *User) Login(ctx context.Context, req *pb.LoginRequest, res *pb.LoginResponse) error {
+	span := opentracing.SpanFromContext(ctx)
+	span.SetOperationName("login")
 
 	if req.Email == "" {
 		return merr.BadRequest(config.ServiceName, "%s", errors.EmailMissing.Error())
@@ -109,16 +126,20 @@ func (u *User) Login(ctx context.Context, req *pb.LoginRequest, res *pb.LoginRes
 	}
 
 	user := &pbUser.UserDetails{
-		Email: req.Email,
+		Email:    req.Email,
 		Password: req.Password,
 	}
+
+	span.LogKV("event", "call userClient.Auth")
 
 	// login
 	response, err := u.userClient.Auth(ctx, user)
 	if err != nil {
-	    log.Debug().Interface("error", err).Str("email", req.Email).Msgf("user login failed")
-	    return err
+		log.Debug().Interface("error", err).Str("email", req.Email).Msgf("user login failed")
+		span.LogKV("event", "auth failed")
+		return err
 	}
+	span.LogKV("event", "auth suceeded")
 
 	res.Token = response.Token
 
@@ -127,6 +148,9 @@ func (u *User) Login(ctx context.Context, req *pb.LoginRequest, res *pb.LoginRes
 
 func (u *User) Videos(ctx context.Context, req *pb.VideoRequest, res *pb.VideoResponse) error {
 	md, _ := metadata.FromContext(ctx)
+
+	span := opentracing.SpanFromContext(ctx)
+	span.SetOperationName("get videos")
 
 	// check for token
 	token, err := u.jwtService.GetBearerToken(md)
@@ -146,13 +170,16 @@ func (u *User) Videos(ctx context.Context, req *pb.VideoRequest, res *pb.VideoRe
 		UserId: claims.User.Id,
 	}
 
+	span.LogKV("event", "call userClient.GetByUserId")
 	// fetch videos
 	videos, err := u.videoClient.GetByUserId(ctx, userRequest)
 	if err != nil {
+		span.LogKV("event", "videos could not be fetched")
 		log.Debug().Interface("error", err).Str("user", claims.User.Id).Msg("unable to fetch videos for user")
-	    return err
+		return err
 	}
 
+	span.LogKV("event", "videos fetched")
 	res.Videos = videos.Videos
 
 	return nil
